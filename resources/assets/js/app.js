7004aac1224f5f8376ca2f9d93a0bd35e2ba74b0
epona <muraka3tada4@icloud.com>
2017-09-18 23:52:26 +0900

    ElementUI CSS

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,6 +6,7 @@ window.Vue = require('vue');
  */
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-default/index.css'
 Vue.use(ElementUI, {locale})
 
 Vue.component('main-contents', require('./components/MainContents.vue'))
--- resources/assets/js/components/MainContents.vue
+++ resources/assets/js/components/MainContents.vue
@@ -11,3 +11,8 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "../../sass/variables";
+
+</style>
